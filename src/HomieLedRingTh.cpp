9e32b075f7104574ce51bec48d3a4ba023560131
Ian Hubbertz <ian_web1@hubbertz.de>
2019-10-13 01:37:23 +0200

    Tuning colors and implementing more MQTT functionality

--- src/HomieLedRingTh.cpp
+++ src/HomieLedRingTh.cpp
@@ -7,6 +7,7 @@
 
 #include <HomieLedRingTh.h>
 #include <Homie.hpp>
+#include <LoggerNode.h>
 
 
 //Why????? (--> https://stackoverflow.com/questions/32433554/c11-uniform-initialization-field-initializer-is-not-constant)
@@ -15,9 +16,11 @@ const CRGB HomieLedRingTh::ModeColor[AUTO_ON+1] {CRGB::Beige, CRGB::Blue, CRGB::
 HomieLedRingTh::HomieLedRingTh():
 	HomieNode("lthermo", "Thermostat", "settemp"),
 	setTemp(210),
+	actTemp(200),
 	mode(UNKNOWN)
 {
 	advertise("settemp").setDatatype("float").setName("Soll-Temperatur").setUnit("°C").settable();
+	advertise("acttemp").setDatatype("float").setName("IST-Temperatur").setUnit("°C").settable();
 	advertise("brightness").setDatatype("int").setName("Helligkeit des LED-Ring").setUnit("percent").settable();
 
 }
@@ -33,37 +36,81 @@ void HomieLedRingTh::loop() {
 void HomieLedRingTh::setup() {
 	FastLED.addLeds<NEOPIXEL, DataPIN>(ledring, TotalLED);
 	FastLED.setBrightness(50);
+	FastLED.setCorrection(TypicalPixelString);
 	updateLEDRing();
 }
 
 bool HomieLedRingTh::handleInput(const HomieRange &range, const String &property, const String &value) {
+	LN.logf(__PRETTY_FUNCTION__, LoggerNode::DEBUG, "Received input for property %s, value %s", property.c_str(), value.c_str());
 	if (property.equals("settemp")) {
 		float v  = value.toFloat();
-		if (v > 15.0 && v < 30.0) {
-			setTemp = roundf(v*10.0);
+		if (v >= (MinTemp/10) && v <= (MaxTemp/10)) {
+			setSetTemp(roundf(v*10.0));
+		} else {
+			LN.logf(__PRETTY_FUNCTION__, LoggerNode::ERROR, "Received value %.2f out of range", v);
 		}
-		setProperty("settemp").setRetained(true).send(String(static_cast<float>(setTemp)/10.0, 1));
+		return true;
+	} else if (property.equals("acttemp")) {
+		float v = value.toFloat();
+		if (v > (MinTemp/10) && v < (MaxTemp/10)) {
+			setActTemp(roundf(v * 10.0));
+		} else {
+			LN.logf(__PRETTY_FUNCTION__, LoggerNode::ERROR, "Received value %.2f out of range", v);
+		}
 		return true;
 	} else if (property.equals("brightness")) {
 		uint_fast8_t v = value.toInt();
 		if (v>100) return false;
 		FastLED.setBrightness(v);
 		setProperty("brightness").send(String(v));
+		updateLEDRing();
+		return true;
 	}
 
 	return false;
 }
 
+int16_t HomieLedRingTh::getSetTemp() const {
+	return setTemp;
+}
+
+void HomieLedRingTh::setSetTemp(int16_t _setTemp) {
+	setTemp = _setTemp;
+	setProperty("settemp").setRetained(true).send(String(static_cast<float>(setTemp)/10.0, 1));
+	updateLEDRing();
+}
+
+void HomieLedRingTh::setActTemp(int16_t actTemp) {
+	this->actTemp = actTemp;
+	setProperty("acttemp").setRetained(true).send(String(static_cast<float>(actTemp)/10.0, 1));
+	updateLEDRing();
+}
+
 void HomieLedRingTh::updateLEDRing() {
 	ledring[LED_CONNECTED] =  Homie.isConnected() ? CRGB::Green : CRGB::Red;
 	ledring[LED_MODE] = ModeColor[mode];
-	for (uint_fast8_t i = 0; i<LedCount; i++) {
+
+	int_fast8_t highlight = 10*(setTemp-MinTemp) / DegPerLED;
+	if (highlight >= LedCount) highlight = LedCount-1;
+	Serial.printf("Highlight-LED: %d\n", highlight);
+	for (int_fast8_t i = 0; i<LedCount; i++) {
 		uint_fast8_t hue = HUE_BLUE - ((HUE_BLUE - HUE_RED) / (LedCount-1))*i;
-		CHSV hC(hue, 255, 255);
+		uint_fast8_t sat = 140;
+		uint_fast8_t bright = 70;
+		// Pixels for actual te1mp
+		if ((MinTemp + (DegPerLED * i)/10) <= actTemp) {
+			sat = (i == highlight) ? 160:255;
+			bright +=100;
+		} else if (i == highlight) {  // Highlight above ActTemp
+			sat -=20;
+		}
+		if (i == highlight) bright = 255;
+
+		Serial.printf("i: %02d T-Start: %d --> hsv: (%03d, %03d, %03d)\n", i, MinTemp + (DegPerLED * i)/10, hue, sat, bright);
+		CHSV hC(hue, sat, bright);
 		int_fast8_t led_no = LED_TSTART + i;
 		if (led_no < 0) led_no+=TotalLED;
 		ledring[led_no] = hC;
 	}
-
 	FastLED.show();
 }
--- src/HomieLedRingTh.h
+++ src/HomieLedRingTh.h
@@ -15,6 +15,9 @@
 class HomieLedRingTh: public HomieNode {
 public:
 	HomieLedRingTh();
+	int16_t getSetTemp() const;
+	void setSetTemp(int16_t setTemp);
+	void setActTemp(int16_t actTemp);
 
 protected:
 	virtual void onReadyToOperate() override;
@@ -27,19 +30,20 @@ private:
 	static const uint8_t TotalLED = 24;
 	CRGB ledring[TotalLED];
 
-	enum ELED { LED_CONNECTED = 11 , LED_MODE = 13, LED_TSTART = -8, LED_TEND = 8 };
+	enum ELED { LED_CONNECTED = 11 , LED_MODE = 13, LED_TSTART = -9, LED_TEND = 9 };
 
-	uint16_t setTemp;		// scaling 1 bit = 0,1°C
-	static const uint16_t MinTemp = 180;		// scaling 1 bit = 0,1°C
-	static const uint16_t MaxTemp = 250;		// scaling 1 bit = 0,1°C
+	int16_t setTemp;		// scaling 1 bit = 0,1°C
+	int16_t actTemp;		// scaling 1 bit = 0,1°C
+	static const int16_t MinTemp = 180;		// scaling 1 bit = 0,1°C
+	static const int16_t MaxTemp = 250;		// scaling 1 bit = 0,1°C
 
 	enum EThermMode { UNKNOWN = 0, MANUAL_OFF, MANUAL_ON, AUTO_OFF, AUTO_ON };
-	static const CRGB ModeColor[AUTO_ON+1];
+	static const CRGB ModeColor[AUTO_ON + 1];
 	EThermMode mode;
 
 
-	static const uint8_t LedCount = LED_TEND-LED_TSTART + 1;
-	static const uint16_t DegPerLED = (MaxTemp-MinTemp) / LedCount;
+	static const int8_t LedCount = LED_TEND - LED_TSTART + 1;
+	static const int16_t DegPerLED = (10*(MaxTemp - MinTemp)) / LedCount; // scaling 1 bit = 0,1°C
 
 	void updateLEDRing();
 
